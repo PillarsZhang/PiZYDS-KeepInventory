af3364343c59ab5c6e00faa0453d180f11f569e4
章鱼DS <zhangkaihao2000@qq.com>
2017-06-16 09:20:58 -0500

    First Try

--- src/PiZYDS-KeepInventory/PiZYDS-KeepInventory.php
+++ src/PiZYDS-KeepInventory/PiZYDS-KeepInventory.php
@@ -1,2 +1,18 @@
 <?php
 namespace PiZYDS-KeepInventory;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\player\PlayerDeathEvent;
+
+class PiZYDS-KeepInventory extends PluginBase{
+public function onEnable(){
+    $this->getServer()->getLogger()->info(C::GREEN."PiZYDS-KeepInventory by PillarsZhang.");
+    $this->setKeepInventory(true);
+    $this->getServer()->getPluginManager()->registerEvents($this, $this);}
+  
+public function onDisable(){
+    $this->getLogger()->info(C::RED . "PiZYDS-KeepInventory have been disabled.");
+    $this->setKeepInventory(false);
+    $this->getServer()->getPluginManager()->registerEvents($this, $this);}
+
+}
